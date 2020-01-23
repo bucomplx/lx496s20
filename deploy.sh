f642070a73cc4f5e2af77ba0048fd5f1cd560ff2
Paul Hagstrom <hagstrom@bu.edu>
2020-01-23 00:42:22 -0500

    No reason to even have deploy.sh here, so remove it.

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#rsync -rvz --delete _site/ hagstrom@ling.bu.edu:/data/wordpress/ling-blogs/static/lx394s19
-# this is obsolete for the github version of this, I used it as a script to push it to our ling-blogs server.
